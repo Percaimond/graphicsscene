b5f19c149c49872efcb0156db5cb42e81191aa18
Daniel <danielweissenborn@outlook.de>
2021-09-27 17:47:02 +0200

    size of displayed image now proportionally to window size

--- otherstuff/basicstructure/utility/pixelarium.js
+++ otherstuff/basicstructure/utility/pixelarium.js
@@ -12,17 +12,6 @@ function display(){
     preview.appendChild(this.img);
 }
 
-function addToCart(){
-  //
-  var imgSource = document.createElement('img');
-  imgSource.src  = picPIC.getElementsByTagName('img')[0].src; 
-  console.log(imgSource.src);
-  imgSource.style.width = "50px";
-  //imgSource.style.marginTop = "20%";
-  $('.box').append(imgSource);
-  console.log(imgSource);
-
-}
 
 function openSite(evt, siteName){
   switch(siteName){
@@ -32,7 +21,7 @@ function openSite(evt, siteName){
       break;
     case 'Utility': window.location.href='utility.html';
       break;
-    case 'YourSongs': window.location.href='music.html';
+    case 'YourSongs': window.location.href='youtube.html';
       break;
     case 'Premium': window.location.href='premium.html';
       break;
@@ -62,7 +51,8 @@ $('.pixel-image').hover(function()
           if(data) {
               var imgSource = document.createElement('img');
               imgSource.src = 'img/' + data.articles[displayINT].picID;
-              imgSource.style.width = "250px";
+              //imgSource.style.width = "250px";
+              imgSource.style.objectFit = "contain";
               $('.picPIC').append(imgSource);
               $('.picID').append("</td> pictureID: </tr>"+ data.articles[displayINT].id);
               $('.picNAME').append("</td> name of the picture: </tr>"+ data.articles[displayINT].name);
@@ -86,6 +76,56 @@ $('.pixel-image').hover(function()
         $('.story').empty();
         $('.price').empty();
         $picName = null;
+    }
+);
+
+/*function addToCart(){
+  //
+  var imgSource = document.createElement('img');
+  imgSource.src  = picPIC.getElementsByTagName('img')[0].src; 
+  console.log(imgSource.src);
+  imgSource.style.width = "50px";
+  $picName picPIC.getElementById
+  $price
+  $('.box').append(imgSource);
+  var imgName = document.createElement('div3');
+  var imgPrice = document.createElement('div4');
+
+
+  imgName.style.backgroundColor  = "black";
+ 
+  imgPrice.style.backgroundColor  = "black";
+  $('.box').append(imgName);
+  $('.box').append(imgPrice);
+
+}*/
+$('addbutton').click(function()
+    {
+      var displayID2 = this.id;
+      var displayINT2 = parseInt(displayID);
+      //$('#box3').removeClass('hidden');
+      $.ajax({
+        type: 'GET',
+        url: 'http://127.0.0.1:5500/otherstuff/basicstructure/utility/articles.json',
+        dataType:"json",
+        success: function(data) { 
+          if(data) {
+            var imgSource = document.createElement('img');
+            imgSource.src  = picPIC.getElementsByTagName('img')[0].src; 
+              imgSource.style.width = "50px";
+
+              $('.box').append(imgSource);
+              $('.box').append("</td> name of the picture: </tr>"+ data.articles[displayINT2].name);
+              $('.box').append("</td>price:  </tr>"+ data.articles[displayINT2].price);
+              console.log(data.articles[displayINT2].id);
+              console.log(imgSource);
+            }
+          },
+          error: function(jqXHR, textStatus, errorThrown){
+            alert('error: ' + textStatus + ': ' + errorThrown);
+          }
+      });
+      return false;
     }
 );
 
